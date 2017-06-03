5c16847e79b83feb03d8fca0d0b75dc3c42e4823
Lakmal Caldera <lakmal.developer@gmail.com>
2017-06-03 20:31:20 +0530

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // POST http method to create a resource
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
 
   var todo = new Todo({
     text: req.body.text
@@ -28,3 +28,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000!');
 });
+
+module.exports = {
+  app
+};
