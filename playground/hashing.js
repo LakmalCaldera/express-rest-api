d54ccab73f2e9b443e9b5869e515e0b2afaab335
Lakmal Caldera <lakmal.developer@gmail.com>
2017-06-10 14:28:40 +0530

    hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,9 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
