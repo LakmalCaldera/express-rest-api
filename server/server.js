f1a1801aad9c84fd3cc9c48c7fa0a03f5fc6c7d6
Lakmal Caldera <lakmal.developer@gmail.com>
2017-06-10 08:41:56 +0530

    added POST /users route and token methods

--- server/server.js
+++ server/server.js
@@ -109,7 +109,23 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   });
+});
 
+// POST /users
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  var user = new User({
+    email: body.email,
+    password: body.password
+  });
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(port, () => {
